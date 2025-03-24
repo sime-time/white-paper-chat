c2a49bed1ba11caa4246706956e7c6ae62050a9e
Dunn, Simeon <simedunn01@gmail.com>
2025-03-24 11:08:56 -0400

    get users from db

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,5 +1,8 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
+import { user } from "./schema/auth-schema";
+import { User } from "better-auth";
+import { eq } from "drizzle-orm";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("database url not found");
@@ -7,3 +10,15 @@ if (!process.env.DATABASE_URL) {
 
 const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle({ client: sql });
+
+export async function getAllUsers() {
+  "use server";
+  const users: User[] | null = await db.select().from(user);
+  return users;
+}
+
+export async function getUser(id: string) {
+  "use server";
+  const singleUser = await db.select().from(user).where(eq(user.id, id));
+  return singleUser
+}
