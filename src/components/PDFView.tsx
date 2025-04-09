7ec9f2e2468deed43b1be6db0723d37798efed56
Dunn, Simeon <simedunn01@gmail.com>
2025-04-09 16:37:50 -0400

    heading for empty pdfview

--- src/components/PDFView.tsx
+++ src/components/PDFView.tsx
@@ -2,6 +2,11 @@ interface PDFViewProps {
   pdfUrl: string;
 }
 export default function PDFView(props: PDFViewProps) {
+  if (!props.pdfUrl) {
+    return (
+      <div>Click to on an chat</div>
+    );
+  }
   return (
     <iframe
       src={props.pdfUrl}
